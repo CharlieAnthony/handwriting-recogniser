f8ea6bc961420a3ea914f4113d74ed5febb579c9
Charlie Anthony <62426743+CharlieAnthony@users.noreply.github.com>
2024-08-18 15:20:12 +0100

    using POST request

--- config/handreader/views.py
+++ config/handreader/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def mainPage(request):
+    if request.method == "POST":
+        print("post")
     return render(request, 'upload.html')
+
+
